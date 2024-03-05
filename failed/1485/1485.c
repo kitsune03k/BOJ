bb0688debe0810b7cf1bc8e1a911d92d36cc5fad
Jeff Lee <kitsune03@outlook.com>
2024-03-06 01:46:50 +0900

    Update 1485.c

--- failed/1485/1485.c
+++ failed/1485/1485.c
@@ -2,115 +2,64 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-typedef struct{
-    int x;
-    int y;
-} Pos;
 
-long double mypow(long double x, long double n)
-{
-    long double ret = 1.0;
+long double calcdis(long double cntx, long double cnty, int px, int py) {
 
-    for(int i=0; i<n; i++){
-        ret *= x;
+}
+
+bool issquare(long long arr[]) {
+    long double cntx, cnty;
+    
+    for(int i=0; i<8; i++){
+
     }
 
-    return ret;
+    for(int i = 0; i < 8; i++) {
+        if(i % 2 == 0) {
+            cntx += (long double) arr[i] / 4.0;
+        }
+        else {
+            cnty += (long double) arr[i] / 4.0;
+        }
+    }
+
+    long double dis[4];
+
+    for(int i = 0; i < 4; i++) {
+        dis[i] = calcdis(cntx, cnty, arr[2 * i], arr[2 * i + 1]);
+        printf("%Lf\n", dis[i]);
+    }
+
+    // filter 1. 중점으로부터의 거리가 모두 같냐?
+    if(
+            (float) dis[0] == (float) dis[1] &&
+            (float) dis[1] == (float) dis[2] &&
+            (float) dis[2] == (float) dis[3] &&
+            (float) dis[3] == (float) dis[0]
+            ) {return true; // 내마음대로 정밀도 줄임, long double 연산시 부동소수점 오류때문에..
+    }
+    else {
+        return false;
+    }
+
 }
 
-int compare(const void* x, const void* y)
-{
-    if(*(int*)x>*(int*)y)
-        return 1;
-    else
-        return -1;
-}
-
-
-int main(void)
-{
-    int T;
-    scanf("%d", &T);
+int main(void) {
+    int t;
+    scanf("%d", &t);
     getchar();
 
-    int answer[T];
-    for(int i=0; i<T; i++){
-        answer[i] = true;
-    }
+    bool ans[t];
 
-    Pos pos[4];
-
-    bool filter[2];
-
-    int xsum=0, ysum=0;
-    long double cx, cy;
-    long double sqcdis[4];
-
-    unsigned long long sqdis[6];
-
-    for(int try = 0; try<T; try++){
-        for(int i=0; i<4; i++){
-            scanf("%d %d", &pos[i].x, &pos[i].y);
-            xsum += pos[i].x;
-            ysum += pos[i].y;
+    long long arr[8];
+    for(int i = 0; i < t; i++) {
+        for(int j = 0; j < 8; j++) {
+            scanf("%lld", &arr[j]);
             getchar();
         }
 
-
-        // filter 1
-        filter[0] = true;
-
-        cx = (long double)xsum / 4.0;
-        cy = (long double)ysum / 4.0;
-
-        for(int i=0; i<4; i++){
-            sqcdis[i] = mypow(
-                    (cx-(long double)pos[i].x), 2) + mypow((cy-(long double)pos[i].y), 2);
-        }
-
-        for(int i=0; i<3; i++){
-            if(sqcdis[i] != sqcdis[i+1]){
-                filter[0] = false;
-                break;
-            }
-        }
-
-
-        // filter 2
-        filter[1] = true;
-
-        int idx = 0;
-        for(int i=0; i<3; i++){
-            for(int j=i+1; j<4; j++){
-                sqdis[idx] =
-                        (int)(
-                                mypow((pos[i].x - pos[j].x), 2) +
-                                mypow((pos[i].y - pos[j].y), 2)
-                        );
-                idx++;
-            }
-        }
-
-        qsort(sqdis, 6, sizeof(unsigned long long), compare);
-
-        for(int i=0; i<3; i++){
-            int cmprslt = sqdis[i] == sqdis[i+1] ? true : false;
-
-            if(cmprslt == 0){
-                filter[1] = false;
-                break;
-            }
-        }
-
-
-        if(filter[0] == false || filter[1] == false)
-            answer[try] = false;
+        printf("%d", issquare(arr));
     }
 
-    for(int i=0; i<T; i++){
-        printf("%d\n", answer[i]);
-    }
-
-    return 0;
 
 }
